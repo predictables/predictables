9f1b0975fd20308ad4a0532ddae6eadfa48e4dc0
Andy Weaver <andrewayersweaver@gmail.com>
2023-12-06 14:22:39 -0500

    kl divergence tests

--- PredicTables/util/stats/src/kl_divergence.py
+++ PredicTables/util/stats/src/kl_divergence.py
@@ -1,13 +1,24 @@
-# Function to calculate the KL divergence between two distributions
-def kl_divergence(observed: list, modeled: list):
+from typing import Union
+
+import numpy as np
+import pandas as pd
+import polars as pl
+
+from PredicTables.util import to_pd_s
+
+
+def kl_divergence(
+    observed: Union[list, pd.Series, pl.Series, np.ndarray],
+    modeled: Union[list, pd.Series, pl.Series, np.ndarray],
+) -> float:
     """Calculate the KL divergence between two distributions. This function is a wrapper for scipy.stats.entropy.
 
     Parameters
     ----------
-    observed : list
-        List of observed values
-    modeled : list
-        List of modeled values
+    observed : Union[list, pd.Series, pl.Series, np.ndarray]
+        Observed values. Must be the same length as modeled values.
+    modeled : Union[list, pd.Series, pl.Series, np.ndarray]
+        Modeled values. Must be the same length as observed values.
 
     Returns
     -------
@@ -15,6 +26,10 @@ def kl_divergence(observed: list, modeled: list):
         KL divergence between the two distributions
     """
     from scipy.stats import entropy
+
+    # Convert to pandas Series
+    observed = to_pd_s(observed)
+    modeled = to_pd_s(modeled)
 
     # Calculate KL divergence directly from observed and modeled averages
     return entropy(observed, modeled)
