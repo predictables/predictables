06e1d4b6349838becfc0c25e32c185a1e34155ff
aaweaver-actuary <andrewayersweaver+github@gmail.com>
2024-01-17 10:18:58 -0500

    initial commit

--- predictables-ui/components/_drawer/AccordionChild.tsx
+++
@@ -1,9 +0,0 @@
-interface AccordionChildProps {
-  children: React.ReactNode;
-}
-
-const AccordionChild = ({ children }: AccordionChildProps) => {
-  return <div>{children}</div>;
-};
-
-export default AccordionChild;
--- predictables-ui/components/_utf-char/GreenCheck.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-// renders the utf-8 character for a green checkmark
-const GreenCheck = () => {
-  return <span className="text-green-500">&#x2713;</span>;
-};
-
-export default GreenCheck;
