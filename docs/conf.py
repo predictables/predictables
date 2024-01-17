e364a61966a5be5fdb7d80e00ca8ed9cebb5c0d1
aaweaver-actuary <andrewayersweaver+github@gmail.com>
2024-01-17 05:23:32 -0500

    fully set up to start building docs in a new branch

--- docs/conf.py
+++ docs/conf.py
@@ -6,23 +6,30 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'PredicTables'
-copyright = '2024, Andy Weaver'
-author = 'Andy Weaver'
-release = '0.0.1'
+project = "PredicTables"
+copyright = "2024, Andy Weaver"
+author = "Andy Weaver"
+release = "0.0.1"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.napoleon",
+    "sphinx.ext.intersphinx",
+    "sphinx.ext.todo",
+    "sphinx.ext.coverage",
+    "sphinx.ext.mathjax",
+    "sphinx.ext.githubpages",
+]
 
-templates_path = ['_templates']
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
-
+templates_path = ["_templates"]
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
-html_static_path = ['_static']
+html_theme = "alabaster"
+html_static_path = ["_static"]
