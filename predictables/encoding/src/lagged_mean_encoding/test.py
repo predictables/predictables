b13d5bda6eb81f377ac531b529e8dd098b4794dd
aaweaver-actuary <andrewayersweaver@gmail.com>
2024-03-12 19:39:20 +0000

    sync up

---
+++ predictables/encoding/src/lagged_mean_encoding/test.py
@@ -0,0 +1,8 @@
+from predictables.encoding.src.lagged_mean_encoding._dynamic_rolling_mean import DynamicRollingMean
+from predictables.encoding.src.lagged_mean_encoding._dynamic_rolling_sum import DynamicRollingSum
+
+__all__ = ["DynamicRollingMean", "DynamicRollingSum"]
+
+df = pl.scan_parquet("cancer_train.parquet")
+
+print(df.head())
