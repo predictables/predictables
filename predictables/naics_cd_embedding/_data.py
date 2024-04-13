ecbda02259bea940a6e792cc06df278201856d63
aaweaver-actuary <andrewayersweaver+github.com>
2024-04-13 10:54:49 +0000

    data loader

--- predictables/naics_cd_embedding/_data.py
+++ predictables/naics_cd_embedding/_data.py
@@ -2,13 +2,25 @@ import torch
 
 
 class NAICSDataLoader(torch.utils.data.DataLoader):
-    """A data loader for the NAICS embedding model."""
+    """A data loader for the NAICS embedding model.
 
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
+    Each iteration of the dataloader should return a tuple of the form:
+    (train_X, train_y, val_X, val_y)
+    """
+
+    def __init__(
+        self,
+        dataset: torch.utils.data.Dataset,
+        batch_size: int = 32,
+        shuffle: bool = True,
+    ):
+        super(NAICSDataLoader, self).__init__(
+            dataset, batch_size=batch_size, shuffle=shuffle
+        )
 
     def __iter__(self):
-        return iter(self.dataset)
+        for i in range(len(self)):
+            yield self.dataset[i]
 
     def __len__(self):
         return len(self.dataset)
