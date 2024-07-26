face95f6ad80e62977398808dd7153122da7cca3
Andy Weaver <andrewayersweaver@gmail.com>
2024-07-26 06:42:16 -0400

    forgot to pass base feature transformer through the __init__.py file

--- predictables/core/src/feature_transformers/__init__.py
+++ predictables/core/src/feature_transformers/__init__.py
@@ -1,3 +1,6 @@
+from predictables.core.src.feature_transformers.base_feature_transformer import (
+    BaseFeatureTransformer,
+)
 from predictables.core.src.feature_transformers.log_transformer import LogTransformer
 
-__all__ = ["LogTransformer"]
+__all__ = ["BaseFeatureTransformer", "LogTransformer"]
