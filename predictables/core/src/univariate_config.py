14dd5ce48c640557abf32a2cabf518e37461bd14
Andy Weaver <andrewayersweaver@gmail.com>
2024-07-26 04:30:16 -0400

    added methods in UnivariateConfig to return a generator with X_train, X_test, y_train, y_test

--- predictables/core/src/univariate_config.py
+++ predictables/core/src/univariate_config.py
@@ -2,8 +2,9 @@
 
 from __future__ import annotations
 from dataclasses import dataclass
-from typing import List
+from typing import List, Generator, Tuple
 import polars as pl
+import pandas as pd
 
 
 @dataclass
@@ -38,3 +39,49 @@ class UnivariateConfig:
     def df_val(self, df: pl.LazyFrame) -> None:
         """Set the validation data."""
         self.df_val = df
+
+    @property
+    def features(self) -> List[str]:
+        """Return the feature column names."""
+        return self.feature_column_names
+
+    @property
+    def X_y_by_cv(self) -> Generator[tuple[pd.DataFrame, pd.DataFrame], None, None]:
+        """Return the training and validation data as a generator."""
+        for i in range(self.cv_folds.min(), self.cv_folds.max() + 1):
+            df_train, df_val = (
+                self.filter_for_time_series(self.df, i)
+                if self.time_series_validation
+                else self.filter_for_non_time_series(self.df, i)
+            )
+            X_train = (
+                df_train.drop([self.target_column_name, self.cv_column_name])
+                .collect()
+                .to_pandas()
+            )
+            y_train = (
+                df_train.select(self.target_column_name).collect().to_numpy().ravel()
+            )
+            X_test = (
+                df_val.drop([self.target_column_name, self.cv_column_name])
+                .collect()
+                .to_pandas()
+            )
+            y_test = df_val.select(self.target_column_name).collect().to_numpy().ravel()
+            yield X_train, X_test, y_train, y_test
+
+    def filter_for_time_series(
+        self, X: pl.LazyFrame, fold: int
+    ) -> Tuple[pl.LazyFrame, pl.LazyFrame]:
+        """Filter the training and testing data for a particular fold, given time series validation."""
+        return X.filter(pl.col(self.cv_column_name) < fold), X.filter(
+            pl.col(self.cv_column_name) == fold
+        )
+
+    def filter_for_non_time_series(
+        self, X: pl.LazyFrame, fold: int
+    ) -> Tuple[pl.LazyFrame, pl.LazyFrame]:
+        """Filter the training and testing data for a particular fold, given non-time series validation."""
+        return X.filter(pl.col(self.cv_column_name) != fold), X.filter(
+            pl.col(self.cv_column_name) == fold
+        )
